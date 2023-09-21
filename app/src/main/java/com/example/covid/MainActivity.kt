cc862ee03e74746cb15ce2c120f6f5adaef53c7d
j1nxie <rylieeeeexd@gmail.com>
2023-09-21 22:34:20 +0700

    feat: top bar implementation

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -3,20 +3,23 @@ package com.example.covid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,7 +34,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.covid.ui.theme.CovidTheme
@@ -63,6 +68,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.top_bar),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+    )
+}
+
 @Composable
 fun CountBox(
     modifier: Modifier = Modifier,
@@ -73,7 +100,7 @@ fun CountBox(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
 
-    ) {
+        ) {
         Text(
             modifier = Modifier
                 .padding(16.dp)
@@ -156,9 +183,9 @@ fun DisplayGraph(
         modelProducer.setEntries(datasetForModel)
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Card(modifier = Modifier
             .fillMaxWidth()
@@ -220,21 +247,30 @@ fun DisplayGraph(
 fun CovidApp(
     modifier: Modifier = Modifier
 ) {
-    Column {
-        DisplayCount(
-            modifier = Modifier
-                .padding(bottom = 16.dp),
-            cases = 100,
-            deaths = 200,
-        )
-        DisplayGraph(
-            modifier = Modifier
-                .padding(bottom = 32.dp)
-                .fillMaxWidth(),
-        )
-    }
+    LazyColumn {
+        item {
+            TopBar()
+        }
 
+        item {
+            DisplayCount(
+                modifier = Modifier
+                    .padding(bottom = 16.dp),
+                cases = 100,
+                deaths = 200,
+            )
+        }
+
+        item {
+            DisplayGraph(
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth(),
+            )
+        }
+    }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun CovidAppPreview() {
