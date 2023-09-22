ef724e74089bf6193cca35b8f3b6b2e430508e4b
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-09-22 23:32:28 +0700

    refactor(MainActivity): remove unused imports

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -9,13 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
