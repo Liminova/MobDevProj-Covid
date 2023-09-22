8ffaeb7822a777086758ca633ecb9b5d7f684da1
Delnegend <kiennguyen19323@gmail.com>
2023-09-22 23:24:27 +0700

    refactor(GraphCard, GraphCards): :recycle: restructuring

--- app/src/main/java/com/example/covid/ui/sections/GraphCards.kt
+++ app/src/main/java/com/example/covid/ui/sections/GraphCards.kt
@@ -1,23 +1,13 @@
 package com.example.covid.ui.sections
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import com.example.covid.ui.components.GraphCard
-import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollState
-import com.patrykandpatrick.vico.compose.component.shape.shader.fromBrush
-import com.patrykandpatrick.vico.core.DefaultAlpha
-import com.patrykandpatrick.vico.core.chart.line.LineChart
-import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
-import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.patrykandpatrick.vico.core.entry.FloatEntry
 
 private fun generateRandomDataPoints(): List<FloatEntry> {
@@ -35,41 +25,14 @@ private fun generateRandomDataPoints(): List<FloatEntry> {
 fun GraphCards(
     modifier: Modifier = Modifier,
 ) {
-    val refreshDataSet = remember { mutableIntStateOf(0) }
-    val modelProducer = remember { ChartEntryModelProducer() }
-    val datasetForModel = remember { mutableStateListOf(listOf<FloatEntry>()) }
-    val datasetLineSpec = remember { arrayListOf<LineChart.LineSpec>() }
-
-    val scrollState = rememberChartScrollState()
-    LaunchedEffect(key1 = refreshDataSet.intValue) {
-        // TODO: rebuild dataset
-        datasetForModel.clear()
-        datasetLineSpec.clear()
-        datasetLineSpec.add(
-            LineChart.LineSpec(
-                lineColor = Color.Green.toArgb(), lineBackgroundShader = DynamicShaders.fromBrush(
-                    brush = Brush.verticalGradient(
-                        listOf(
-                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                        )
-                    )
-                )
-            )
-        )
-        datasetForModel.add(generateRandomDataPoints())
-        modelProducer.setEntries(datasetForModel)
-    }
-
-    Column(
-        modifier = modifier.fillMaxSize()
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp),
     ) {
-        GraphCard(
-            datasetForModel = datasetForModel,
-            datasetLineSpec = datasetLineSpec,
-            modelProducer = modelProducer,
-            scrollState = scrollState,
-            refreshDataSet = refreshDataSet,
-        )
+        items(4) {
+            GraphCard(generateRandomDataPoints()).New()
+            Spacer(modifier = Modifier.padding(8.dp))
+        }
     }
 }
