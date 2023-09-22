9fd399b978ed849421d0a7f6045e3a172542456b
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-09-22 22:25:36 +0700

    feat: added modalNavigationDrawer composable
    Please bind the menu button

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -1,37 +1,65 @@
 package com.example.covid
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.covid.ui.components.CountCard
 import com.example.covid.ui.components.GraphCard
 import com.example.covid.ui.theme.CovidTheme
@@ -44,6 +72,7 @@ import com.patrykandpatrick.vico.core.chart.line.LineChart
 import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.patrykandpatrick.vico.core.entry.FloatEntry
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +83,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    CovidApp()
+                    CovidAppV2()
                 }
             }
         }
@@ -63,7 +92,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
+fun TopBar(drawerState: DrawerState) {
     CenterAlignedTopAppBar(title = {
         Text(
             text = stringResource(id = R.string.top_bar),
@@ -71,7 +100,9 @@ fun TopBar() {
             overflow = TextOverflow.Ellipsis
         )
     }, navigationIcon = {
-        IconButton(onClick = { /* doSomething() */ }) {
+        IconButton(onClick = {
+//            drawerState.open()
+        }) {
             Icon(
                 imageVector = Icons.Filled.Menu, contentDescription = "Localized description"
             )
@@ -162,39 +193,83 @@ fun DisplayGraph(
     }
 }
 
-@Composable
-fun CovidApp(
-    modifier: Modifier = Modifier
-) {
-    LazyColumn(
-        modifier = modifier
-    ) {
-        item {
-            TopBar()
-        }
-
-        item {
-            DisplayCount(
-                modifier = Modifier.padding(bottom = 16.dp),
-                cases = 789012,
-                deaths = 123456,
-            )
-        }
-
-        item {
-            DisplayGraph(
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth(),
-            )
-        }
-    }
-}
+//@Composable
+//fun CovidApp(
+//    modifier: Modifier = Modifier
+//) {
+//    LazyColumn(
+//        modifier = modifier
+//    ) {
+//        item {
+//            TopBar()
+//        }
+//
+//        item {
+//            DisplayCount(
+//                modifier = Modifier.padding(bottom = 16.dp),
+//                cases = 789012,
+//                deaths = 123456,
+//            )
+//        }
+//
+//        item {
+//            DisplayGraph(
+//                modifier = Modifier
+//                    .padding(bottom = 32.dp)
+//                    .fillMaxWidth(),
+//            )
+//        }
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
-fun CovidAppPreview() {
-    CovidTheme {
-        CovidApp()
-    }
+fun CovidAppV2() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    // icons to mimic drawer destinations
+    val items = listOf(
+        Icons.Default.Favorite,
+        Icons.Default.Face,
+        Icons.Default.Email,
+        Icons.Default.Home
+    )
+    val selectedItem = remember { mutableStateOf(items[0]) }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(Modifier.height(12.dp))
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        icon = { Icon(item, contentDescription = null) },
+                        label = { Text(item.name) },
+                        selected = item == selectedItem.value,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            selectedItem.value = item
+                        },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
+            }
+        },
+        content = {
+            Column {
+                TopBar(drawerState = drawerState)
+                DisplayCount(
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    cases = 789012,
+                    deaths = 123456,
+                )
+                Spacer(Modifier.height(20.dp))
+                DisplayGraph(
+                    modifier = Modifier
+                        .padding(bottom = 32.dp)
+                        .fillMaxWidth(),
+                )
+            }
+        }
+    )
 }
+
