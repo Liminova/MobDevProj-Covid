c7140f6d74f199239b031caf4cac82ed3d8679fc
Delnegend <kiennguyen19323@gmail.com>
2023-09-27 01:44:41 +0700

    feat(GraphSection): :sparkles: preparing for MVVM pattern

---
+++ app/src/main/java/com/example/covid/ui/sections/GraphSection.kt
@@ -0,0 +1,155 @@
+package com.example.covid.ui.sections
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollState
+import com.patrykandpatrick.vico.compose.component.shape.shader.fromBrush
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.core.DefaultAlpha
+import com.patrykandpatrick.vico.core.chart.line.LineChart
+import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.FloatEntry
+
+data class SuccessData(
+    val newCases: List<FloatEntry>,
+    val cumulativeCases: List<FloatEntry>,
+    val newDeaths: List<FloatEntry>,
+    val cumulativeDeaths: List<FloatEntry>,
+)
+
+sealed interface GraphUiState {
+    object Loading : GraphUiState
+    data class Success(val data: SuccessData) : GraphUiState
+    data class Error(val message: String) : GraphUiState
+}
+
+@Composable
+fun GraphSection(
+    graphUiState: GraphUiState, modifier: Modifier = Modifier
+) {
+    when (graphUiState) {
+        is GraphUiState.Loading -> Loading(modifier)
+        is GraphUiState.Success -> Success(graphUiState.data, modifier = modifier)
+        is GraphUiState.Error -> Error(graphUiState.message, modifier)
+    }
+}
+
+@Composable
+private fun GraphCard(
+    modifier: Modifier = Modifier,
+    dataPoints: List<FloatEntry>,
+    onButtonClick: () -> Unit = {},
+    description: String = "This is the default description",
+) {
+    val refreshDataSet = remember { mutableIntStateOf(0) }
+    val modelProducer = remember { ChartEntryModelProducer() }
+    val datasetForModel = remember { mutableStateListOf(listOf<FloatEntry>()) }
+    val datasetLineSpec = remember { arrayListOf<LineChart.LineSpec>() }
+    val scrollState = rememberChartScrollState()
+    LaunchedEffect(key1 = refreshDataSet.intValue) {
+        // TODO: rebuild dataset
+        datasetForModel.clear()
+        datasetLineSpec.clear()
+        datasetLineSpec.add(
+            LineChart.LineSpec(
+                lineColor = Color.Green.toArgb(), lineBackgroundShader = DynamicShaders.fromBrush(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                        )
+                    )
+                )
+            )
+        )
+        datasetForModel.add(dataPoints)
+        modelProducer.setEntries(datasetForModel)
+    }
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Card {
+            if (datasetForModel.isNotEmpty()) {
+                ProvideChartStyle {
+                    Chart(
+                        chart = lineChart(
+                            lines = datasetLineSpec
+                        ),
+                        chartModelProducer = modelProducer,
+                        chartScrollState = scrollState,
+                        isZoomEnabled = true,
+                    )
+                }
+            }
+            TextButton(modifier = Modifier.fillMaxWidth(), onClick = { onButtonClick }) {
+                Text(
+                    text = description,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Success(
+    data: SuccessData,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        GraphCard(
+            Modifier.padding(bottom = 16.dp),
+            dataPoints = data.newCases,
+            description = "New Cases"
+        )
+        GraphCard(
+            Modifier.padding(bottom = 16.dp),
+            dataPoints = data.cumulativeCases,
+            description = "Cumulative Cases"
+        )
+        GraphCard(
+            Modifier.padding(bottom = 16.dp),
+            dataPoints = data.newDeaths,
+            description = "New Deaths"
+        )
+        GraphCard(
+            Modifier.padding(bottom = 16.dp),
+            dataPoints = data.cumulativeDeaths,
+            description = "Cumulative Deaths"
+        )
+    }
+}
+
+@Composable
+private fun Loading(modifier: Modifier = Modifier) {
+    Text(text = "Loading...", modifier = modifier)
+}
+
+@Composable
+private fun Error(message: String, modifier: Modifier = Modifier) {
+    Text(text = message, modifier = modifier)
+}
