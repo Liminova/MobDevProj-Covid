2eed363ab1723ff4003e608a11aa2c16d01a12f1
Delnegend <kiennguyen19323@gmail.com>
2023-09-27 01:45:39 +0700

    feat(LocationCard): :sparkles: add last updated text

--- app/src/main/java/com/example/covid/ui/components/LocationCard.kt
+++ app/src/main/java/com/example/covid/ui/components/LocationCard.kt
@@ -1,17 +1,30 @@
 package com.example.covid.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import com.example.covid.R
 
 @Composable
-fun LocationCard(modifier: Modifier = Modifier, selectedCountry: String = "USA") {
-    Text(
+fun LocationCard(
+    modifier: Modifier = Modifier,
+    countryName: String,
+    lastUpdated: String,
+) {
+    Column(
         modifier = modifier,
-        text = selectedCountry,
-        style = MaterialTheme.typography.displayLarge,
-        fontWeight = FontWeight.Bold
-    )
+    ) {
+        Text(
+            text = countryName,
+            style = MaterialTheme.typography.displayLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.last_updated, lastUpdated),
+        )
+    }
 }
