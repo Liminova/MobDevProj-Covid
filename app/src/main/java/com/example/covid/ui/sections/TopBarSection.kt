391f824037136c090c23d3e074cdeb78cc9a3ac8
Delnegend <kiennguyen19323@gmail.com>
2023-09-28 00:50:00 +0700

    feat(TopBarSection): :sparkles: fade animation

--- app/src/main/java/com/example/covid/ui/sections/TopBarSection.kt
+++ app/src/main/java/com/example/covid/ui/sections/TopBarSection.kt
@@ -1,6 +1,9 @@
 package com.example.covid.ui.sections
 
-import androidx.compose.foundation.ScrollState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -10,7 +13,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.covid.ui.AppViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,22 +28,30 @@ fun TopBarSection(
     appViewModel: AppViewModel,
     onNavIconClicked: () -> Unit,
 ) {
+    var visible by remember { mutableStateOf(false) }
     var title by remember { mutableStateOf("") }
     LaunchedEffect(appViewModel.scrollState.value) {
         val threshold = 350
         if (appViewModel.scrollState.value > threshold) {
             title = appViewModel.selectedCountry.value
+            visible = true
         } else {
+            visible = false
         }
     }
 
     CenterAlignedTopAppBar(
         title = {
-            Text(
-                text = appViewModel.topBarTitle.value,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
+            AnimatedVisibility(
+                visible = visible, enter = fadeIn(), exit = fadeOut()
+            ) {
+                Text(
+                    modifier = Modifier.padding(start = 24.dp, end = 24.dp),
+                    text = title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
         },
         navigationIcon = {
             IconButton(onClick = { onNavIconClicked() }) {
