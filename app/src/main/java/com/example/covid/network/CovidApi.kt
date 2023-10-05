25aafdb21add44f2a18122e3dc056c0c5f9cf1fd
Delnegend <kiennguyen19323@gmail.com>
2023-10-05 15:31:34 +0700

    feat: :sparkles: covidApi retrofit implementation

---
+++ app/src/main/java/com/example/covid/network/CovidApi.kt
@@ -0,0 +1,50 @@
+package com.example.covid.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+private const val BASE_URL = "https://covid.delnegend.com/"
+
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+private val retrofit =
+    Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(BASE_URL)
+        .build()
+
+data class CountryData(
+    @Json(name = "CountryName") val countryName: String,
+    @Json(name = "WHORegion") val whoRegion: String,
+    @Json(name = "Reports") val reports: List<Report>
+)
+
+data class Report(
+    @Json(name = "Date") val date: String,
+    @Json(name = "NewCases") val newCases: Int,
+    @Json(name = "NewDeaths") val newDeaths: Int,
+    @Json(name = "CumulativeCases") val cumulativeCases: Int,
+    @Json(name = "CumulativeDeaths") val cumulativeDeaths: Int
+)
+
+// Defines how the data is retrieved from the endpoint
+// Functions to call in our code to get the data
+// A class but the body is unnecessary -> use interface
+interface CovidApiService {
+    @GET("/countries")
+    suspend fun getCountries(): Map<String, String>
+
+    @GET("/countries/{countryId}")
+    suspend fun getCountryData(@Path("countryId") countryId: String): CountryData
+}
+
+// This makes sure that we only have one object of this class in our app
+// because calling retrofit.create is resource intensive
+object CovidApi {
+    val retrofitService: CovidApiService by lazy {
+        retrofit.create(CovidApiService::class.java)
+    }
+}
