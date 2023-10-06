06a2d520560c6f25d3fefa2221a7e1d9ce9bf7ca
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-10-06 23:36:50 +0700

    refactor: yes

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -3,79 +3,14 @@ package com.example.covid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Surface
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.covid.ui.AppViewModel
-import com.example.covid.ui.components.LocationCard
-import com.example.covid.ui.sections.CountSection
-import com.example.covid.ui.sections.DrawerContent
-import com.example.covid.ui.sections.GraphSection
-import com.example.covid.ui.sections.TopBarSection
-import com.example.covid.ui.theme.CovidTheme
-import kotlinx.coroutines.launch
+import com.example.covid.app.CovidApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CovidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    CovidApp()
-                }
-            }
+            CovidApp()
         }
     }
 }
 
-@Composable
-fun CovidApp() {
-    val appViewModel = viewModel<AppViewModel>()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
-    ModalNavigationDrawer(drawerState = drawerState,
-        drawerContent = { DrawerContent(appViewModel, scope, drawerState) }) {
-        Column {
-            TopBarSection(
-                appViewModel,
-                onNavIconClicked = { scope.launch { drawerState.open() } },
-                onInfoIconClicked = {}
-            )
-            Column(
-                Modifier
-                    .padding(horizontal = 16.dp)
-                    .verticalScroll(appViewModel.scrollState)
-            ) {
-                LocationCard(
-                    Modifier.padding(top = 64.dp, bottom = 96.dp),
-                    appViewModel.selectedCountry.value,
-                    appViewModel.lastUpdated.value,
-                )
-                CountSection(
-                    Modifier.padding(bottom = 16.dp),
-                    appViewModel.totalDeaths.intValue,
-                    appViewModel.totalCases.intValue,
-                    appViewModel.totalCasesLast30d.intValue,
-                    appViewModel.fatalityRate.floatValue,
-                )
-                GraphSection(Modifier.fillMaxWidth(), appViewModel)
-            }
-        }
-    }
-}
---
+++ app/src/main/java/com/example/covid/ui/screens/StatScreen.kt
@@ -0,0 +1,58 @@
+package com.example.covid.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.covid.ui.AppViewModel
+import com.example.covid.ui.components.LocationCard
+import com.example.covid.ui.sections.CountSection
+import com.example.covid.ui.sections.DrawerContent
+import com.example.covid.ui.sections.GraphSection
+import com.example.covid.ui.sections.TopBarSection
+import kotlinx.coroutines.launch
+
+@Composable
+fun StatScreen() {
+    val appViewModel = viewModel<AppViewModel>()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(drawerState = drawerState,
+        drawerContent = { DrawerContent(appViewModel, scope, drawerState) }) {
+        Column {
+            TopBarSection(
+                appViewModel,
+                onNavIconClicked = { scope.launch { drawerState.open() } },
+                onInfoIconClicked = {}
+            )
+            Column(
+                Modifier
+                    .padding(horizontal = 16.dp)
+                    .verticalScroll(appViewModel.scrollState)
+            ) {
+                LocationCard(
+                    Modifier.padding(top = 64.dp, bottom = 96.dp),
+                    appViewModel.selectedCountry.value,
+                    appViewModel.lastUpdated.value,
+                )
+                CountSection(
+                    Modifier.padding(bottom = 16.dp),
+                    appViewModel.totalDeaths.intValue,
+                    appViewModel.totalCases.intValue,
+                    appViewModel.totalCasesLast30d.intValue,
+                    appViewModel.fatalityRate.floatValue,
+                )
+                GraphSection(Modifier.fillMaxWidth(), appViewModel)
+            }
+        }
+    }
+}
