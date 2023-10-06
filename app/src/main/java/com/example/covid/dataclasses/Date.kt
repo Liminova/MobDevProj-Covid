e457fa3e65c670b2683f3c425aa22484e4b238ed
Delnegend <kiennguyen19323@gmail.com>
2023-10-06 19:14:40 +0700

    feat(Date): :sparkles: add toInt and toEpochDay

--- app/src/main/java/com/example/covid/dataclasses/Date.kt
+++ app/src/main/java/com/example/covid/dataclasses/Date.kt
@@ -12,4 +12,18 @@ data class Date(
     fun toFloat(): Float {
         return (year * 10000 + month * 100 + day).toFloat()
     }
+
+    fun toInt(): Int {
+        return (year * 10000 + month * 100 + day)
+    }
+
+    fun toEpochDay(): Long {
+        val daysInMonth = when (month) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            4, 6, 9, 11 -> 30
+            2 -> 28
+            else -> 0
+        }
+        return (year * 365 + month * daysInMonth + day).toLong()
+    }
 }
