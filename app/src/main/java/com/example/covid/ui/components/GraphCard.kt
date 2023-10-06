fc655c86e8f107bcef7c9757fa0c7eaa7f0580a2
Delnegend <kiennguyen19323@gmail.com>
2023-10-06 19:14:57 +0700

    refactor(GraphCard): :recycle: simplify everything

--- app/src/main/java/com/example/covid/ui/components/GraphCard.kt
+++ app/src/main/java/com/example/covid/ui/components/GraphCard.kt
@@ -2,15 +2,12 @@ package com.example.covid.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -30,88 +27,92 @@ import com.patrykandpatrick.vico.core.DefaultAlpha
 import com.patrykandpatrick.vico.core.axis.AxisItemPlacer
 import com.patrykandpatrick.vico.core.chart.line.LineChart
 import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
+import com.patrykandpatrick.vico.core.entry.ChartEntry
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
-import com.patrykandpatrick.vico.core.entry.FloatEntry
+
+class CovidEntry(
+    private val date: Date,
+    private val cases: Int,
+    override val x: Float = date.toEpochDay().toFloat(),
+    override val y: Float = cases.toFloat(),
+) : ChartEntry {
+    override fun withY(y: Float): ChartEntry {
+        return CovidEntry(date, cases, x, y)
+    }
+}
 
 @Composable
 fun GraphCard(
     modifier: Modifier = Modifier,
-    dataPoints: Map<Date, Float>,
+    dataPoints: Map<Date, Int>,
     onButtonClick: () -> Unit = {},
     description: String,
 ) {
-    val refreshDataSet = remember { mutableIntStateOf(0) }
-    val modelProducer = remember { ChartEntryModelProducer() }
-    val datasetForModel = remember { mutableStateListOf(listOf<FloatEntry>()) }
-    val datasetLineSpec = remember { arrayListOf<LineChart.LineSpec>() }
     val scrollState = rememberChartScrollState()
-
-    LaunchedEffect(key1 = refreshDataSet.intValue) {
-        datasetForModel.clear()
-        datasetLineSpec.clear()
-        datasetLineSpec.add(
-            LineChart.LineSpec(
-                lineColor = Color.Green.toArgb(), lineBackgroundShader = DynamicShaders.fromBrush(
-                    brush = Brush.verticalGradient(
-                        listOf(
-                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                            Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                        )
+    val datasetLineStyle = arrayListOf(
+        LineChart.LineSpec(
+            lineColor = Color.Green.toArgb(), lineBackgroundShader = DynamicShaders.fromBrush(
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                        Color.Green.copy(DefaultAlpha.LINE_BACKGROUND_SHADER_END)
                     )
                 )
             )
         )
-        val dataset = dataPoints.map { (date, value) ->
-            FloatEntry(
-                x = date.toFloat(),
-                y = value
-            )
-        }
-        datasetForModel.add(dataset)
-        modelProducer.setEntries(datasetForModel)
-    }
+    )
+    val modelProducer =
+        ChartEntryModelProducer(dataPoints.map { (key, value) -> CovidEntry(key, value) })
     Column(
         modifier = modifier.fillMaxWidth()
     ) {
         Card {
-            if (datasetForModel.isNotEmpty()) {
-                ProvideChartStyle {
-                    val marker = rememberMarker()
-                    Chart(
-                        chart = lineChart(
-                            lines = datasetLineSpec
-                        ),
-                        chartModelProducer = modelProducer,
-
-                        // AXIS:
-                        startAxis = rememberStartAxis(
-                            title = "Cases", tickLength = 0.dp, valueFormatter = { value, _ ->
-                                ((value.toInt()) + 1).toString()
-                            }, itemPlacer = AxisItemPlacer.Vertical.default(
-                                maxItemCount = 6,
-                            )
-                        ),
-
-                        bottomAxis = rememberBottomAxis(
-                            title = "Date",
-                            tickLength = 0.dp,
-                            valueFormatter = { value, _ ->
-                                value.toString()
-                            },
-                        ),
-
-                        // MARKER:
-                        marker = marker,
-                        chartScrollState = scrollState,
-                        isZoomEnabled = true,
-                    )
-                }
+            if (dataPoints.isEmpty()) {
+                Text(
+                    text = "No data available",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                return@Card
             }
+            ProvideChartStyle {
+                val marker = rememberMarker()
+                Chart(
+                    chart = lineChart(lines = datasetLineStyle),
+                    chartModelProducer = modelProducer,
+
+                    // AXIS:
+                    startAxis = rememberStartAxis(
+                        title = "Cases", tickLength = 0.dp, valueFormatter = { value, _ ->
+                            ((value.toInt()) + 1).toString()
+                        }, itemPlacer = AxisItemPlacer.Vertical.default(
+                            maxItemCount = 6,
+                        )
+                    ),
+
+                    bottomAxis = rememberBottomAxis(
+                        title = "Date",
+                        tickLength = 0.dp,
+                        valueFormatter = { value, _ ->
+                            value.toString()
+                        },
+                    ),
+
+                    // MARKER:
+                    marker = marker,
+                    chartScrollState = scrollState,
+                    isZoomEnabled = true,
+                )
+            }
+
             TextButton(modifier = Modifier.fillMaxWidth(), onClick = { onButtonClick }) {
                 Text(
                     text = description,
                     textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp, 16.dp)
                 )
             }
         }
