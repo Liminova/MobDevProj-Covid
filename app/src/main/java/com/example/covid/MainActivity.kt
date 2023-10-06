d8d2089ddeba55c5e6ab591a8bdcfdf49f2e1df3
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-10-06 23:30:10 +0700

    fix: i forgor

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -4,40 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.covid.ui.AppViewModel
 import com.example.covid.ui.components.LocationCard
 import com.example.covid.ui.sections.CountSection
+import com.example.covid.ui.sections.DrawerContent
 import com.example.covid.ui.sections.GraphSection
 import com.example.covid.ui.sections.TopBarSection
 import com.example.covid.ui.theme.CovidTheme
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -88,52 +75,6 @@ fun CovidApp() {
                     appViewModel.fatalityRate.floatValue,
                 )
                 GraphSection(Modifier.fillMaxWidth(), appViewModel)
-            }
-        }
-    }
-}
-
-@Composable
-fun DrawerContent(
-    appViewModel: AppViewModel, scope: CoroutineScope, drawerState: DrawerState
-) {
-    var searchQuery by remember { mutableStateOf("") }
-    val filteredCountries =
-        appViewModel.countriesListMap.filter { it.value.contains(searchQuery, true) }
-    ModalDrawerSheet {
-        Spacer(Modifier.height(12.dp))
-        TextField(
-            value = searchQuery,
-            onValueChange = { searchQuery = it },
-            label = { Text("Search") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(rememberScrollState())
-        ) {
-            if (appViewModel.readyToComposeCountryList.intValue == 0) {
-                NavigationDrawerItem(label = { Text("loading...") },
-                    selected = false,
-                    onClick = { })
-                return@ModalDrawerSheet
-            }
-            val temp = filteredCountries.ifEmpty { appViewModel.countriesListMap }
-            temp.forEach { (countryCode, countryName) ->
-                NavigationDrawerItem(
-                    label = { Text(countryName) },
-                    selected = countryName == appViewModel.selectedCountry.value,
-                    onClick = {
-                        scope.launch {
-                            drawerState.close()
-                            appViewModel.updateCountry(countryCode, countryName)
-                        }
-                    },
-                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
-                )
             }
         }
     }
